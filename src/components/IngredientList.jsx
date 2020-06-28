617ce314e2c9d8ac97a008f408c5178ed99cdf42
pedscn <pedscn@gmail.com>
2020-06-28 16:45:14 +0200

    Added cards and flex container for ingredients

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -1,15 +1,41 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import recipesFile from "../recipedb.json";
+import placeholderImage from "../placeholder-img.png";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 200,
+    maxHeight: 200,
+  },
+  flexcontainer: {
+    height: "400px",
+    display: "flex",
+    "flex-wrap": "wrap",
+    "overflow-y": "scroll",
+  },
+});
 
 const IngredientList = () => {
+  const classes = useStyles();
   const ingredientMappedList = recipesFile.map((recipe) => (
-    <li>{recipe.Ingredient01}</li>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia component="img" image={placeholderImage} />
+        <CardContent>
+          <Typography gutterBottom variant="h6">
+            {recipe.Ingredient01}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   ));
-  return (
-    <div>
-      <ul>{ingredientMappedList}</ul>
-    </div>
-  );
+  return <div className={classes.flexcontainer}>{ingredientMappedList}</div>;
 };
 
 export default IngredientList;
