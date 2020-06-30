6c717e02f2c6c95d8468ed03cec607de9ca2e01b
pedscn <pedscn@gmail.com>
2020-06-30 16:31:56 +0200

    Added selectedIngredients prop to RecipeList.jsx

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Recipe from "./Recipe";
 import recipesFile from "../recipedb.json";
 
-const RecipeList = () => {
+const RecipeList = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const { selectedIngredients } = props;
   const recipeMappedList = recipesFile.map((recipe) => (
     <Recipe title={recipe.Title} directions={recipe.Directions} />
   ));
   return <ul>{recipeMappedList}</ul>;
+};
+
+RecipeList.propTypes = {
+  selectedIngredients: PropTypes.instanceOf(Array),
+};
+
+RecipeList.defaultProps = {
+  selectedIngredients: [],
 };
 
 export default RecipeList;
